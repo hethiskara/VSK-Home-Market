866d807d01e4bd7a34c93d17888927ebf8571fd8
Hethiskarna Ravi <hethiskarnar@gmail.com>
2026-04-02 22:53:22 -0700

    fix

--- src/config/buildConfig.js
+++ src/config/buildConfig.js
@@ -1,5 +1,5 @@
 // This file is auto-updated during build process
 // Build date is set when generating APK
-// Last updated: 2026-03-04
+// Last updated: 2026-04-03
 
-export const BUILD_DATE = '2026-03-04';
+export const BUILD_DATE = '2026-04-03';
